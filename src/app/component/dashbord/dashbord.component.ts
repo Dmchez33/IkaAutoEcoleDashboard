af80900e93fd2cd4023cad5ecfe8e7437f1768e7
Idrissa Dembélé <iddembele206@gmail.com>
2023-02-27 06:47:39 +0000

    avant dernier commit

--- src/app/component/dashbord/dashbord.component.ts
+++ src/app/component/dashbord/dashbord.component.ts
@@ -9,6 +9,7 @@ import { CoursService } from 'src/app/service/cours/cours.service';
 import { QuizService } from 'src/app/service/quiz/quiz.service';
 import { UtilisateurService } from 'src/app/service/utilisateur/utilisateur.service';
 import { AutoecoleService } from 'src/app/service/autoecole/autoecole.service';
+import * as L from 'leaflet';
 
 Leaflet.Icon.Default.imagePath = 'assets/';
 
@@ -26,14 +27,45 @@ export class DashbordComponent implements OnInit {
   allApprenant: any;
   allUserConnected:any;
   autoecole:any;
-  constructor(private serviceautoecole: AutoecoleService,private serviceUtilisateur: UtilisateurService,private serviceCours: CoursService,private serviceQuiz: QuizService) { }
+  autoecoleLatLng = new Array();
+  map!: L.Map;
+  markers: L.Marker[] = [];
+  markerGroup!: L.LayerGroup;
+
+  constructor(private autoecoleService: AutoecoleService,private serviceUtilisateur: UtilisateurService,private serviceCours: CoursService,private serviceQuiz: QuizService) { }
 
   ngOnInit(): void {
-    this.serviceautoecole.getAllAutoEcole().subscribe(data =>{
-      console.log(data)
-      this.autoecole = data;
-    })
-
+    
+    this.initMap();
+    this.autoecoleService.getAllAutoEcole().subscribe(autoecoles => {
+      for (let i = 0; i < autoecoles.length; i++) {
+        const autoecole = autoecoles[i];
+        const lat = Number(autoecole.adresses[0].latitude);
+        const lng = Number(autoecole.adresses[0].longitude);
+        const ville = autoecole.adresses[0].ville;
+        const quartier = autoecole.adresses[0].quartier;
+        const nomAutoecole = autoecole.nom
+        this.addMarker(lng,lat,ville,quartier,nomAutoecole);
+      }
+      const group = L.featureGroup(this.markers).addTo(this.map);
+      this.map.fitBounds(group.getBounds());
+    });
+      // for(let i=0; i<data.length; i++){
+      //   this.autoecoleLatLng.push(
+      //     {
+      //       position: { lat: Number(data[i].adresses[0].latitude), lng: Number(data[i].adresses[0].longitude) },
+      //       draggable: true,
+      //       ville: data[i].adresses[0].ville,
+      //       quartier: data[i].adresses[0].quartier,
+      //       nomAutoecole:data[i].nom
+      //     }
+      //   );
+      // }
+      
+      // console.log(this.autoecoleLatLng); // afficher les valeurs de autoecoleLatLng
+      // this.initMarkers(); // initialiser les marqueurs sur la carte
+    // })
+    console.log(this.autoecoleLatLng[0])
     this.serviceCours.listeAllCours().subscribe(data => {
       this.allCoursLength = data.length
       console.log(data.length)
@@ -64,162 +96,75 @@ export class DashbordComponent implements OnInit {
         console.log(data);
         this.allUserConnected = data;
       }
-    )
-
+    );
   }
-
-  // @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
-  // public barChartOptions: ChartConfiguration['options'] = {
-  //   responsive: true,
-  //   // We use these empty structures as placeholders for dynamic theming.
-  //   scales: {
-  //     x: {},
-  //     y: {
-  //       min: 0,
-  //       max: 200
-  //     }
-  //   },
-  //   plugins: {
-  //     legend: {
-  //       display: true,
-  //       position: 'bottom'
-  //     },
-  //     datalabels: {
-  //       anchor: 'end',
-  //       align: 'end'
-  //     }
-  //   }
-  // };
-  // public barChartType: ChartType = 'bar';
-  // public barChartPlugins = [
-  //   DataLabelsPlugin
-  // ];
-
-  // public barChartData: ChartData<'bar'> = {
-  //   labels: ['Nombre de d\'auto-écoles', 'Nombre de d\'utilisateurs', 'Nombre de de quiz'],
-  //   datasets: [
-  //     { data: [ 55, 100, 5], label: 'Graphe montrant les statistiques', backgroundColor: '#1A237E',borderColor: '#1A237E', },
-  //     // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  //   ]
-  // };
-
-  // events
-  // public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-  //   console.log(event, active);
-  // }
-
-  // public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-  //   console.log(event, active);
-  // }
-
-  // public randomize(): void {
-  //   // Only Change 3 values
-  //   this.barChartData.datasets[0].data = [
-  //     Math.round(Math.random() * 100),
-  //     59,
-  //     80,
-  //     Math.round(Math.random() * 100),
-  //     56,
-  //     Math.round(Math.random() * 100),
-  //     40];
-
-  //   this.chart?.update();
-  // }
-
-
-  // @ViewChild(BaseChartDirective) Chart: BaseChartDirective | undefined;
-
-  // // Pie
-  // public pieChartOptions: ChartConfiguration['options'] = {
-  //   responsive: true,
-  //   plugins: {
-  //     legend: {
-  //       display: true,
-  //       position: 'bottom',
-  //     },
-  //     datalabels: {
-  //       formatter: (value, ctx) => {
-  //         if (ctx.chart.data.labels) {
-  //           return ctx.chart.data.labels[ctx.dataIndex];
-  //         }
-  //       },
-  //     },
-  //   }
-  // };
-  // public pieChartData: ChartData<'pie', number[], string | string[]> = {
-  //   labels: [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ],
-  //   datasets: [ {
-  //     data: [ 300, 500, 100 ]
-  //   } ]
-  // };
-  // public pieChartType: ChartType = 'pie';
-  // public pieChartPlugins = [ DataLabelsPlugin ];
-
-  // // events
-  // public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
-  //   console.log(event, active);
-  // }
-
-  // public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
-  //   console.log(event, active);
-  // }
-
-
-
-  // code par rapport au map
-  map!: Leaflet.Map;
-  markers: Leaflet.Marker[] = [];
-  options = {
-    layers: [
-      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      })
-    ],
-    zoom: 16,
-    center: {
-       lat: 28.626137, 
-       lng: 79.821603 }
-  }
-
-  initMarkers() {
-    const initialMarkers = [
-      {
-        position: { lat: 28.625485, lng: 79.821091 },
-        draggable: true
-      },
+  private initMap(): void {
+    this.map = L.map('map', {
+      //center: [48.8566, 2.3522],
+      zoom: 6,
       
-    ];
-    for (let index = 0; index < initialMarkers.length; index++) {
-      const data = initialMarkers[index];
-      const marker = this.generateMarker(data, index);
-      marker.addTo(this.map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
-      this.map.panTo(data.position);
-      this.markers.push(marker)
-    }
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
   }
 
-  generateMarker(data: any, index: number) {
-    return Leaflet.marker(data.position, { draggable: data.draggable })
-      .on('click', (event) => this.markerClicked(event, index))
-      .on('dragend', (event) => this.markerDragEnd(event, index));
+  private addMarker(lat: number, lng: number, ville:any,quartier:any,nom:any): void {
+    const marker = L.marker([lat, lng]).addTo(this.map).bindPopup(`<b>Ville: ${ville}</b> <div>Quartier: ${quartier} </div> <div>Auto-école: ${nom} </div>`);;
+  this.markers.push(marker);
   }
+  // code par rapport au map
+  // map!: Leaflet.Map;
+  // markers: Leaflet.Marker[] = [];
+  // options = {
+  //   layers: [
+  //     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  //     })
+  //   ],
+  //   zoom: 16,
+  //   // center: {
+  //   //    lat: 28.626137, 
+  //   //    lng: 79.821603 }
+  // }
 
-  onMapReady($event: Leaflet.Map) {
-    this.map = $event;
-    this.initMarkers();
-  }
+  // initMarkers() {
+  //   const initialMarkers = [
+  //     {
+  //       position: { lat: 28.625485, lng: 79.821091 },
+  //       draggable: true
+  //     },
+      
+  //   ];
+  //   console.log(this.autoecoleLatLng)
+  //   for (let index = 0; index < this.autoecoleLatLng.length; index++) {
+  //     const data = this.autoecoleLatLng[index];
+  //     const marker = this.generateMarker(data, index);
+  //     marker.addTo(this.map).bindPopup(`<b>Ville: ${data.ville}</b> <div>Quartier: ${data.quartier} </div> <div>Auto-école: ${data.nomAutoecole} </div>`);
+  //     this.map.panTo(data.position);
+  //     this.markers.push(marker)
+  //   }
+  // }
 
-  mapClicked($event: any) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
+  // generateMarker(data: any, index: number) {
+  //   return Leaflet.marker(
+  //     data.position, 
+  //     { draggable: data.draggable }
+  //     )
+  //     .on('click', (event) => this.markerClicked(event, index))
+  //     //.on('dragend', (event) => this.markerDragEnd(event, index));
+  // }
 
-  markerClicked($event: any, index: number) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
-
-  markerDragEnd($event: any, index: number) {
-    console.log($event.target.getLatLng());
-  } 
+  // onMapReady($event: Leaflet.Map) {
+  //   this.map = $event;
+  //   this.initMarkers();
+  // }
+  // markerClicked($event: any, index: number) {
+  //   console.log($event.latlng.lat, $event.latlng.lng);
+  // }
 
 }
